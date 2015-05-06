002fecf79ba1f01625c19c527c7f56124e6b9fe4
Daniel A. Thompson <me@danielthompson.net>
2015-05-06 18:05:00 -0500

    comment test

--- src/net/danielthompson/danray/cameras/DepthOfFieldCamera.java
+++ src/net/danielthompson/danray/cameras/DepthOfFieldCamera.java
@@ -29,7 +29,7 @@ public class DepthOfFieldCamera extends CameraBase implements Camera {
    public Point getWorldPointForPixel(double x, double y) {
 
       Point point = getDefaultOrientationWorldPointForPixel(x, y);
-
+      // comment test
       ConvertToWorldCoordinates(point);
 
       return point;
