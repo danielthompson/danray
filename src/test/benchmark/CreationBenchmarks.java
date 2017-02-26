b01208a9d1fe5b623e2ca49618fbad692438011c
Daniel A. Thompson <github@danielthompson.net>
2017-02-25 21:31:27 -0600

    commented out some not-useful tests, added travis file

--- src/test/benchmark/CreationBenchmarks.java
+++ src/test/benchmark/CreationBenchmarks.java
@@ -26,38 +26,38 @@ public class CreationBenchmarks {
    public void tearDown() throws Exception {
 
    }
-
-   @Test
-   public void test() throws Exception {
-      for (int i = 1; i < 10; i++) {
-         test1MIntersectionStates((int)(1000000*(5 * i)));
-      }
-   }
-
-   @Test
-   public void test1MIntersectionStates(int n) throws Exception {
-
-
-
-      IntersectionState[] array = new IntersectionState[n];
-
-      final long startTime = System.currentTimeMillis();
-
-      for (int i = 0; i < n; i++) {
-         array[i] = new IntersectionState();
-      }
-
-      final long endTime = System.currentTimeMillis();
-
-      final long duration = endTime - startTime;
-
-      long rate = (n / duration) * 1000;
-
-      DecimalFormat formatter = new DecimalFormat("#,###");
-
-      System.out.println(formatter.format(rate) + " intersectionstates / sec, [" + n + "] objects, duration = [" + duration + "] ms");
-
-      System.out.println("");
-
-   }
+//
+//   @Test
+//   public void test() throws Exception {
+//      for (int i = 1; i < 10; i++) {
+//         test1MIntersectionStates((int)(1000000*(5 * i)));
+//      }
+//   }
+//
+//   @Test
+//   public void test1MIntersectionStates(int n) throws Exception {
+//
+//
+//
+//      IntersectionState[] array = new IntersectionState[n];
+//
+//      final long startTime = System.currentTimeMillis();
+//
+//      for (int i = 0; i < n; i++) {
+//         array[i] = new IntersectionState();
+//      }
+//
+//      final long endTime = System.currentTimeMillis();
+//
+//      final long duration = endTime - startTime;
+//
+//      long rate = (n / duration) * 1000;
+//
+//      DecimalFormat formatter = new DecimalFormat("#,###");
+//
+//      System.out.println(formatter.format(rate) + " intersectionstates / sec, [" + n + "] objects, duration = [" + duration + "] ms");
+//
+//      System.out.println("");
+//
+//   }
 }
