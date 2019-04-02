ee6bff88f0fef1963b13ead4464cd5130e0acb69
Daniel A. Thompson <github@danielthompson.net>
2019-04-02 06:55:59 -0500

    Added javadoc to Logger

--- src/main/java/net/danielthompson/danray/Logger.java
+++ src/main/java/net/danielthompson/danray/Logger.java
@@ -13,38 +13,74 @@ import java.util.Calendar;
 import java.util.Date;
 
 /**
- * Created by daniel on 1/3/15.
+ * Logging mechanism. By default, logs to a stdout and a file in the current trace directory.
  */
 public class Logger {
 
+   /**
+    * Severity of the logged event.
+    */
    public enum Level {
+      /**
+       * Informational events.
+       */
       Info(1),
+
+      /**
+       * Recoverable errors that do not cause program shutdown.
+       */
       Warning(2),
+
+      /**
+       * Unrecoverable errors that cause program shutdown.
+       */
       Error(3),
+
+      /**
+       * Diagnostic-level events useful for debugging. Use caution; this logs a ton of information and
+       * slows the rendering process considerably.
+       */
       Debug(4);
 
       private int _value;
 
+      /**
+       * Create a new Level enum with the specified level.
+       * @param value
+       */
       Level(int value) {
          _value = value;
       }
-
-      public int getNumVal() {
-         return _value;
-      }
    }
 
-   public static Level LogLevel = Logger.Level.Error;
+   /**
+    * The current diagnostic level.
+    */
+   static Level LogLevel = Logger.Level.Error;
 
+   /**
+    * The date format to use for log messages.
+    */
    private static SimpleDateFormat sdf = new SimpleDateFormat("[yyyy-MM-dd hh:mm:ss.SSS] ");
 
+   /**
+    * The list of output streams to log to. By default, contains stdout and a file in the trace directory.
+    */
    private static final ArrayList<PrintStream> _streams = new ArrayList<PrintStream>();
 
-   public static void AddOutput(PrintStream stream) {
+   /**
+    * Add the given PrintStream to the list of output streams.
+    * @param stream The PrintStream to add to the list of output streams.
+    */
+   static void AddOutput(PrintStream stream) {
       _streams.add(stream);
    }
 
-   public static void AddOutputFile(File file) {
+   /**
+    * Adds a PrintStream created from the given file to the list of output streams.
+    * @param file The output File to add to the list of output streams.
+    */
+   static void AddOutputFile(File file) {
       try {
          PrintStream stream = new PrintStream(file);
          _streams.add(stream);
@@ -55,6 +91,11 @@ public class Logger {
       }
    }
 
+   /**
+    * Log the given message at the given severity level.
+    * @param level The severity level of the event.
+    * @param text The event text.
+    */
    public static void Log(Level level, String text) {
       if (level._value <= LogLevel._value) {
          Date date = Calendar.getInstance().getTime();
