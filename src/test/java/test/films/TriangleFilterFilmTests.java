5167dffa9f0297008288b06a59d582a9b0d85690
Daniel A. Thompson <github@danielthompson.net>
2019-04-12 06:08:40 -0500

    Added tests

--- src/test/java/test/films/TriangleFilterFilmTests.java
+++ src/test/java/test/films/TriangleFilterFilmTests.java
@@ -2,11 +2,16 @@ package test.films;
 
 import net.danielthompson.danray.films.AbstractFilm;
 import net.danielthompson.danray.films.TriangleFilterFilm;
+import net.danielthompson.danray.shading.SpectralPowerDistribution;
+import net.danielthompson.danray.structures.Sample;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.SampleModel;
 
 import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 
@@ -26,7 +31,18 @@ public class TriangleFilterFilmTests {
       BufferedImage image = new BufferedImage(2, 2, TYPE_INT_RGB);
       AbstractFilm film = new TriangleFilterFilm(image);
 
-      film.AddSamples(1, 1, );
+      Sample sample = new Sample();
+      sample.SpectralPowerDistribution = new SpectralPowerDistribution();
+      sample.SpectralPowerDistribution.R = 1.0f;
+      sample.SpectralPowerDistribution.G = 1.0f;
+      sample.SpectralPowerDistribution.B = 1.0f;
 
+      Sample[] samples = new Sample[] {sample};
+
+      film.AddSamples(1.5f, 1.5f, samples);
+
+      Color color = new Color(film.Image.getRGB(1, 1));
+
+      Assert.assertEquals(color, new Color(255, 255, 255));
    }
 }
