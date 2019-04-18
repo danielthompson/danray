198ba7b1edaffbf07d497271a4bc5273f4c5b84d
Daniel A. Thompson <github@danielthompson.net>
2019-04-18 10:52:39 -0500

    Added test

--- src/test/java/test/shapes/csg/DifferenceTests.java
+++ src/test/java/test/shapes/csg/DifferenceTests.java
@@ -3,10 +3,8 @@ package test.shapes.csg;
 import net.danielthompson.danray.shapes.Box;
 import net.danielthompson.danray.shapes.csg.CSGOperation;
 import net.danielthompson.danray.shapes.csg.CSGShape;
-import net.danielthompson.danray.structures.Point;
-import net.danielthompson.danray.structures.Ray;
-import net.danielthompson.danray.structures.Transform;
-import net.danielthompson.danray.structures.Vector;
+import net.danielthompson.danray.states.Intersection;
+import net.danielthompson.danray.structures.*;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -75,5 +73,29 @@ public class DifferenceTests {
 
       boolean hits = shape.Hits(ray);
       Assert.assertTrue(hits);
+   }
+
+   @Test
+   public void shouldHit2() {
+      Point origin = new Point(0, 0, -2);
+      Vector direction = new Vector(2, 0, 5);
+      Ray ray = new Ray(origin, direction);
+
+      boolean hits = shape.Hits(ray);
+
+      Intersection intersection = shape.GetHitInfo(ray);
+
+      Normal expectedNormal = new Normal(0, 0, -1);
+      Point expectedHitPoint = new Point(1.6f, 0, 2);
+
+      Assert.assertNotNull(intersection);
+      Assert.assertTrue(intersection.Hits);
+
+      Assert.assertNotNull(intersection.Normal);
+      Assert.assertEquals(intersection.Normal, expectedNormal);
+
+      Assert.assertNotNull(intersection.Location);
+      Assert.assertEquals(intersection.Location, expectedHitPoint);
+
    }
 }
