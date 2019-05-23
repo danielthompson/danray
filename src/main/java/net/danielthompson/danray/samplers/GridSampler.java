5fbb55bfcd7833ec167c4f62998008c7b9c77ff8
Daniel A. Thompson <github@danielthompson.net>
2019-05-23 15:01:33 -0500

    fixed point2

--- src/main/java/net/danielthompson/danray/samplers/GridSampler.java
+++ src/main/java/net/danielthompson/danray/samplers/GridSampler.java
@@ -8,20 +8,14 @@ import net.danielthompson.danray.structures.Point2;
 public class GridSampler extends AbstractSampler {
 
    private int rootNumSamples;
-
    private float differential;
-
    private float halfDifferential;
 
    public GridSampler(int samplesPerPixel) {
       super(samplesPerPixel);
-
       rootNumSamples = (int)Math.sqrt(samplesPerPixel);
-
       SamplesPerPixel = rootNumSamples * rootNumSamples;
-
       differential = 1.0f / (rootNumSamples);
-
       halfDifferential = differential * 0.5f;
    }
 
@@ -29,18 +23,16 @@ public class GridSampler extends AbstractSampler {
    public Point2[] GetSamples(int x, int y, int n) {
 
       Point2[] pixels = new Point2[SamplesPerPixel];
-
       int index;
-
       float xOffset = x + halfDifferential;
       float yOffset = y + halfDifferential;
 
       for (int i = 0; i < rootNumSamples; i++) {
          index = i * rootNumSamples;
+         float yDiff = differential * i;
 
          for (int j = 0; j < rootNumSamples; j++) {
-            pixels[index + j].x = xOffset + differential * j;
-            pixels[index + j].y = yOffset + differential * i;;
+            pixels[index + j] = new Point2(xOffset + differential * j, yOffset + yDiff);
          }
       }
 
--- src/main/java/net/danielthompson/danray/samplers/RandomSampler.java
+++ src/main/java/net/danielthompson/danray/samplers/RandomSampler.java
@@ -3,6 +3,8 @@ package net.danielthompson.danray.samplers;
 import net.danielthompson.danray.structures.Point2;
 import net.danielthompson.danray.utility.GeometryCalculations;
 
+import java.util.SplittableRandom;
+
 /**
  * Created by daniel on 5/15/16.
  */
@@ -16,9 +18,10 @@ public class RandomSampler extends AbstractSampler {
    public Point2[] GetSamples(int x, int y, int n) {
       Point2[] pixels = new Point2[n];
 
+      SplittableRandom random = GeometryCalculations.splitRandom.get();
+
       for (int i = 0; i < n; i++) {
-         pixels[i].x = (float) (x + GeometryCalculations.splitRandom.get().nextDouble());
-         pixels[i].y = (float) (y + GeometryCalculations.splitRandom.get().nextDouble());
+         pixels[i] = new Point2((float) (x + random.nextDouble()), (float) (y + random.nextDouble()));
       }
 
       return pixels;
